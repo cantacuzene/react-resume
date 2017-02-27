d6d7a71315bf1c4532eec7a0fe95dd77a2553e37
Hugo Cantacuzene <hugo.cantacuzene@zags.com>
2017-02-27 19:17:09 +0100

    reactify app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import styles from "./styles/stylesheet.scss"
+import React from 'react';
+import {render} from 'react-dom';
+//import {createStore, combineReducers} from 'redux'
+import {Provider} from 'react-redux';
+import {Router,Route, browserHistory,IndexRoute} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
+import styles from "./styles/stylesheet.scss";
+import configureStore from './store/configureStore';
+import App from './components/App';
+import HomePage from './components/HomePage';
 
+const store = configureStore();
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store);
 
+render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <IndexRoute component={HomePage} />
+            </Route>
+        </Router>
+  </Provider>, document.getElementById('app')
+);
+
+/*
 let changeInnerHtml = function(){
     let body = document.getElementsByTagName("body")[0];
 
@@ -10,4 +33,4 @@ let changeInnerHtml = function(){
 
 document.addEventListener("DOMContentLoaded",function(){
   //  changeInnerHtml()
-});
+});*/
