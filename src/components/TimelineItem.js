3b5c78647a74f5950f1e2022aaab1e3843427ba3
Hugo Cantacuzene <h.cantacuzene@gmail.com>
2018-02-28 14:38:48 -0400

    added timeline to homepage

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,16 +1,54 @@
 import React from 'react'
 
 class TimelineItem extends React.Component{
+
     render(){
+
+        let stack=null;
+        if(this.props.stack)
+        {
+            stack=<p className> <h3>Stack:</h3>
+                    {this.props.stack}</p>
+        }
+
+        let scrum=null;
+        if(this.props.scrum)
+        {
+            scrum=<p className> <h3>Scrum Master role:</h3>
+                    {this.props.scrum}</p>
+        }
+        let architect=null;
+        if(this.props.architect)
+        {
+            architect=<p className> <h3>Architect role:</h3>
+                    {this.props.architect}</p>
+        }
+        let description=null;
+        if(this.props.description)
+        {
+            description=<p className> <h3>Description:</h3>
+                    {this.props.description}</p>
+        }
+        let operations=null;
+        if(this.props.operations)
+        {
+            description=<p className> {this.props.operations}</p>
+        }
         return (
-            <li>
-                <div className="TimelineItem">
-                    <h3>{this.props.title}</h3>
-                    <p>
-                        {this.props.description}
-                    </p>
+
+            <div className="timeline-item">
+                <div className="timeline-icon"></div>
+                <div  className={ `timeline-content ${this.props.contentClassName}` }>
+                    <h2>
+                        {this.props.start} - {this.props.end} <br/> {this.props.title}, {this.props.company}
+                    </h2>
+                    {description}
+                    {scrum}
+                    {architect}
+                    {operations}
+                    {stack}
                 </div>
-            </li>
+            </div>
 
         );
     }
--- src/containers/Timeline.js
+++ src/containers/Timeline.js
@@ -3,19 +3,22 @@ import TimelineItem from '../components/TimelineItem'
 
 const Timeline = ()=>{
     return(
-        <section className="timeline">
-            <ul>
-            <TimelineItem title="zags" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
-             <TimelineItem title="zags" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
-              <TimelineItem title="zags" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
-               <TimelineItem title="zags" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
-             <TimelineItem title="zags" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
-              <TimelineItem title="zags" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
+        <div className="container">
+        <section id="timeline">
+
+            <TimelineItem title="Software Architect" company="Zags!" start="04/2015" end="05/2017" 
+            description="Scrum master, puis Architecte chez un éditeur de logiciel franco-américain spécialisé dans le domaine de l'assurance" 
+            scrum=" suivi de reporting, garant des cérémonies agiles, référent technique et manager d’une équipe de 5 personnes. Maintenance et évolutions sur le scope fonctionnel group life EMEA de la Zags Suite."
+            architect="participation à la définition et à la mise en place d’une architecture CQS pour les nouveaux modules basée sur des services Restful combinés à un front React. Participation aux ateliers de définition du besoin fonctionnel. Mise en place des chaines de build/release linux/docker pour dotnet Core et javascript dans VSTS. Référent technique pour les équipes de développement. Javascript & git evangelist."
+            stack="asp.Net Mvc 4, .Net 4.5.2, WCF, Visual studio 2015, Roslyn, VSTS, Moq, Xunit, TDD/DDD, OAuth 2.0, Azure, JS, React, Scrum, .Net Core, VS Code, VS 2017, docker, CI/CD, TFS/GIT"/>
+             <TimelineItem contentClassName="right" title="Product Owner" company="MGEN" start="07/2014" end="03/2015" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
+              <TimelineItem title="Lead Software Engineer" company="Natixis Asset Management" start="04/2010" end="07/2014" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
                <TimelineItem title="zags" description="Lorem Ipsumfsdfsdfsdfsdf  sdfsdfsdf sdfsfdfsd sdfsdfsdfsdf sdfsdfsdfsdf  sdfsdfdsf"/>
 
-            </ul>
+
             
         </section>
+        </div>
     );
 }
 
