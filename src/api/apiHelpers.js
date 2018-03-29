af1123034d48b5c58cd1627eabdfd76513ae9d1f
Hugo Cantacuzene <h.cantacuzene@gmail.com>
2018-03-29 16:48:05 -0400

    removed unnecessary console.log

--- src/api/apiHelpers.js
+++ src/api/apiHelpers.js
@@ -3,7 +3,6 @@ import {conf} from '../config/config'
 var fetchf = Future.encaseP(fetch);
 
 export const getRessourcesWithConf=(conf)=>(ressourcePath)=> {
-    console.log(`fetching ...http://${conf.host}:${conf.port}/api/${ressourcePath}`);
 return fetchf(  `http://${conf.host}:${conf.port}/api/${ressourcePath}`)
 .chain(res => Future.tryP(() => 
 {
