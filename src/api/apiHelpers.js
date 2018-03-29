21e22123333f3f252b6f8b1e6287088c1b20db6d
Hugo Cantacuzene <h.cantacuzene@gmail.com>
2018-03-29 13:04:39 -0400

    Merge branch 'testingwithwallaby.js'
    added conole.log for fetching purposes

--- src/api/apiHelpers.js
+++ src/api/apiHelpers.js
@@ -4,12 +4,12 @@ var fetchf = Future.encaseP(fetch);
 
 export const getRessourcesWithConf=(conf)=>(ressourcePath)=> {
     console.log(`fetching ...http://${conf.host}:${conf.port}/api/${ressourcePath}`);
-    return fetchf(  `http://${conf.host}:${conf.port}/api/${ressourcePath}`)}
+return fetchf(  `http://${conf.host}:${conf.port}/api/${ressourcePath}`)
 .chain(res => Future.tryP(() => 
 {
     return res.json()
 }
-));
+))};
 
 
 
